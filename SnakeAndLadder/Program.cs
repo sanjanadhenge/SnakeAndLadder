c5ac6a155e926eb16b4837299415c5c6dbf21fec
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-03-06 13:08:01 +0530

    [sanjana] Add : Snake and Ladder game played with single playerat start postion 0 and Welcome message

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace SnakeAndLadder
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to SnakeAndLadder Game");
             SnakeLadder snakeLadder = new SnakeLadder();    
         }
     }
